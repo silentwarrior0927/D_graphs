e58bf31860002a96ef5b797291406e44258ccaf5
Silent Warrior <silentwarrior0927@outlook.com>
2014-06-08 21:59:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DUMACGraphs::Application.config.secret_key_base = '42c2c925190111dd1aa610cfbd9b387f04584edad6b0df7c4f1b231afc91820acd62806e3597332fd52058330c0588ad97071162d9319b986349a44ae8d5714c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DUMACGraphs::Application.config.session_store :cookie_store, key: '_DUMAC_graphs_session'
